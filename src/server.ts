413b71ca7cac5afa1f0dc41f3bb476603d38fa4e
Vinicius Alberti <73914923+albertivini@users.noreply.github.com>
2021-06-27 14:21:25 -0300

    Instalando CORS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata'
 import express, { Request, Response, NextFunction } from 'express'
 import "express-async-errors"
+import cors from "cors"
 
 import { router } from "./routes"
 
 import './database' // já importa o arquivo index.ts dentro
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
